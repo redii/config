b6316f0a811bcb0e8292936ff2096e8f75f32924
akmann <akmann.henry@gmail.com>
2021-02-24 14:28:15 +0100

    adjusted hyper.js css

--- macos/home/.hyper.js
+++ macos/home/.hyper.js
@@ -67,7 +67,7 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '14px 14px',
+    padding: '0px 14px 14px 14px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
