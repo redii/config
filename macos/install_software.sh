bd9ee4e4e045294dde7e35201034e626496d63e6
akmann <akmann.henry@gmail.com>
2022-03-25 11:26:59 +0100

    updated install script and .zshrc

--- macos/install_software.sh
+++ macos/install_software.sh
@@ -3,11 +3,10 @@
 # install homebrew package manager
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# install additional software
+# install cli software
 brew install zsh
 brew install git
-brew install node
-brew install deno
+brew install nvm
 brew install python@3.7
 brew install rust
 brew install docker
@@ -25,28 +24,30 @@ brew install --cask firefox
 brew install --cask brackets
 brew install --cask visual-studio-code
 brew install --cask hyper
-brew install --cask google-photos-backup-and-sync
-brew install --cask adobe-creative-cloud
+brew install --cask google-drive
 brew install --cask docker
 brew install --cask insomnia
 brew install --cask cyberduck
-brew install --cask keepingyouawake
 brew install --cask spotify
 brew install --cask hiddenbar
 brew install --cask utm
 brew install --cask bitwarden
 brew install --cask tunnelblick
+brew install --cask postico
 
 # install cask fonts
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code
 brew install --cask font-jetbrains-mono
 
+# install node using nvm
+nvm install --lts
+
 # install node modules
-npm i -g pure-prompt
-npm i -g npx
-npm i -g nodemon
-npm i -g markdown-pdf
-npm i -g eslint
-npm i -g http-server
-npm i -g caprover
+npm install --global pure-prompt
+npm install --global npx
+npm install --global nodemon
+npm install --global markdown-pdf
+npm install --global eslint
+npm install --global http-server
+npm install --global caprover
