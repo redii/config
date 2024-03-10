b7b6d7eb7cd7d0a1dab21c028910d4ecf4cab539
Henry Akmann <akmann.henry@gmail.com>
2024-03-10 20:12:22 +0100

    updated install_software.sh

--- install_software.sh
+++ install_software.sh
@@ -15,7 +15,8 @@ brew install hugo                         # hugo SSG cli
 brew install youtube-dl                   # download videos
 brew install macfuse                      # third-party filesystem support
 brew install rclone                       # mount and sync data 
-brew install ffmpeg			  # audio and video editing
+brew install ffmpeg               			  # audio and video editing
+brew install bruno                        # http/api client
 
 # install cask software
 brew install --cask google-chrome
@@ -26,16 +27,16 @@ brew install --cask docker
 brew install --cask visual-studio-code
 brew install --cask obsidian              # markdown editor
 brew install --cask alfred                # advanced spotlight search
+brew install --cask bartender             # hide items from menubar
 brew install --cask unnaturalscrollwheels # fix mouse and trackpad scrolling
 brew install --cask command-x             # cut files in finder
 brew install --cask aldente               # prevent overcharging
 brew install --cask tailscale             # networking made easy
 brew install --cask tunnelblick           # vpn client
-brew install --cask mullvadvpn            # vpn mullvad
+brew install --cask mullvadvpn            # vpn by mullvad
 brew install --cask vpn-by-google-one     # vpn by google
 brew install --cask warp                  # ai powered cli
 brew install --cask powershell            # powershell for mac
-brew install --cask insomnia              # develop and test apis
 brew install --cask cyberduck             # ftp client
 brew install --cask notunes               # prevent iTunes/Apple Music
 
@@ -43,6 +44,7 @@ brew install --cask notunes               # prevent iTunes/Apple Music
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code
 brew install --cask font-jetbrains-mono
+brew install --cask poppins
 
 # install node using nvm
 nvm install --lts                       # install latest node lts
