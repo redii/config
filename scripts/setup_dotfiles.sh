3bfa1f55b0d3fcfa67065a24625b93bd7dcf90ad
akmann <akmann@proton.me>
2026-01-20 08:00:12 +0100

    added ssh aliases

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ln -s $PWD/.gitignore_global ~
-ln -s $PWD/.gitconfig ~
 ln -s $PWD/.zshrc ~
+ln -s $PWD/.ssh ~
+ln -s $PWD/.gitconfig ~
+ln -s $PWD/.gitignore_global ~
 ln -s $PWD/.vimrc ~
 ln -s $PWD/.prettierrc ~
 
